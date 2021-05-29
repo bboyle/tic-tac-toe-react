23c72bd9df03fd3d9ff03bba8eb06066e1bb14dd
Ben Boyle <benjamins.boyle@gmail.com>
2021-05-29 16:40:53 +1000

    basic board component and app layout

---
+++ tic-tac-toe/src/game/Board.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Board.css';
+
+
+class Board extends React.Component {
+  render() {
+    return (
+      <table>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
+    );
+  }
+}
+
+export default Board;
